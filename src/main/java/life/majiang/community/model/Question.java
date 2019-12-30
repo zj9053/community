f50ad197b58f397f7bf55fd8236a65035aa88ea9
zj9053 <905305152@qq.com>
2019-12-30 12:49:14 +0800

    new

---
+++ src/main/java/life/majiang/community/model/Question.java
@@ -0,0 +1,21 @@
+package life.majiang.community.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Question {
+    private Integer id;
+    private String title;
+    private String description;
+    private String tag;
+    private Long gmtCreate;
+    private Long gmtModified;
+    private Integer creator;
+    private Integer viewCount;
+    private Integer commentCount;
+    private Integer likeCount;
+}
