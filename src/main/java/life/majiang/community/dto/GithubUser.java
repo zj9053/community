74175977feb77ed05081381b42cac79b6ca51d71
zj9053 <905305152@qq.com>
2020-01-01 19:06:32 +0800

    new

--- src/main/java/life/majiang/community/dto/GithubUser.java
+++ src/main/java/life/majiang/community/dto/GithubUser.java
@@ -1,40 +1,10 @@
 package life.majiang.community.dto;
 
+import lombok.Data;
+
+@Data
 public class GithubUser {
     private String name;
     private String id;
     private String bio;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
-
-    @Override
-    public String toString() {
-        return "GithubUser{" +
-                "name='" + name + '\'' +
-                ", id='" + id + '\'' +
-                ", bio='" + bio + '\'' +
-                '}';
-    }
 }
--- src/main/java/life/majiang/community/model/User.java
+++ src/main/java/life/majiang/community/model/User.java
@@ -14,4 +14,5 @@ public class User {
     private String token;
     private Long gmtCreate;
     private Long gmtModified;
+    private String avatarUrl;
 }
